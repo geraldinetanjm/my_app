1934189811fc9f094372d6f8d260067e16892bcb
Geraldine Tan <Geraldine@Geraldines-Macbook-Pro-3.local>
2018-12-04 22:08:13 +0800

    Updates styles of homepage

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Avatar } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Avatar    
+  xlarge
+  rounded
+  source={{uri: "file:///Users/Geraldine/Desktop/Gerodyssey/Transparentlogo.png"}}
+  onPress={() => console.log("Geraldine")}
+  activeOpacity={0.7}
+/>
+        <Text style={styles.bigblue}>Good dayy</Text>
+        <Button title={'Hello'} 
+        onPress={() => { console.log ('happy'); }} 
+      color="blue"
+        
+        />
       </View>
     );
   }
 }
 
+  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFD700',
     alignItems: 'center',
     justifyContent: 'center',
   },
+    bigblue: {
+      color: 'black',
+      fontWeight: 'bold',
+      fontSize: 30,
+    },
+  
 });
