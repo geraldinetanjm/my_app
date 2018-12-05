559c1ec68833cf530adee716217b4f57dade1134
Geraldine Tan <Geraldine@Geraldines-Macbook-Pro-3.local>
2018-12-05 20:35:48 +0800

    Creates header component

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { Avatar } from 'react-native-elements'
+import { Avatar } from 'react-native-elements';
+import Header from './components/header';
 
 export default class App extends React.Component {
   render() {
     return (
+      // Containter.
       <View style={styles.container}>
+      <Header textIwant='Welcome' />
+      <Header textIwant={'goodbye'} />
+      <Header />
       <Avatar    
   xlarge
   rounded
-  source={{uri: "file:///Users/Geraldine/Desktop/Gerodyssey/Transparentlogo.png"}}
-  onPress={() => console.log("Geraldine")}
+  source={{ uri: 'file:///Users/Geraldine/Desktop/Gerodyssey/Transparentlogo.png' }}
+  onPress={() => console.log('Geraldine')}
   activeOpacity={0.7}
-/>
-        <Text style={styles.bigblue}>Good dayy</Text>
-        <Button title={'Hello'} 
-        onPress={() => { console.log ('happy'); }} 
+      />
+        <Text style={styles.bigblue}>GERALDINE</Text>
+        <Button
+title={'Hello'} 
+        onPress={() => { console.log('happy'); }} 
       color="blue"
         
         />
@@ -23,13 +29,12 @@ export default class App extends React.Component {
     );
   }
 }
-
   
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFD700',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+// This is a stateless functional component 
+//const Header =(props) => <Text>{ props textIwant }</Text>
+
+//This is stateful functional component
+class Header extends React.Component {
+    constructor(props){ // Set state up
+        super(props); // Run state
+        //How we create properties 
+        this.textIwant=props.textIwant;
+    }
+
+render (){
+    return (
+        <Text>{this.textIwant}</Text>
+    );
+}
+}
+export default Header;
