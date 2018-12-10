9280000f9a74f0e55f3ba2dcb632c560fe9f007d
Geraldine Tan <Geraldine@Geraldines-Macbook-Pro-3.local>
2018-12-10 21:14:33 +0800

    flex box added

--- screen/Todo.js
+++ screen/Todo.js
@@ -6,7 +6,28 @@ class TodoScreen extends React.Component {
         title: 'To Do',
       };
     render() {
-        return (<Text>Todo App</Text>);
+        return (
+            <View style={{ flex: 1, flexDirection: 'colomn' }}>
+        <View 
+            style={{
+                width: 150, 
+                height: 200, 
+                backgroundColor: 'powderblue' }} 
+        />
+        <View 
+            style={{
+                width: 50, 
+                height: 50, 
+                backgroundColor: 'skyblue' }} 
+        />
+        <View  
+            style={{
+                width: 50, 
+                height: 50, 
+                backgroundColor: 'steelblue' }} 
+        />
+      </View>
+        );
     }
 }
 export default TodoScreen;
