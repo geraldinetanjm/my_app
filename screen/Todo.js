c0eb166024eacada5eb1ae2cfdfe5e4d7f6be313
Geraldine Tan <Geraldine@Geraldines-Macbook-Pro-3.local>
2018-12-11 21:56:38 +0800

    Section header component added

--- screen/Todo.js
+++ screen/Todo.js
@@ -1,35 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, FlatList, SectionList } from 'react-native';
 
 class TodoScreen extends React.Component {
     static navigationOptions = {
         title: 'ABOUT',
       };
+    renderRow = ({ item }) => 
+      (<View style={styles.row}>
+        <Text>{item.text}</Text>
+        </View>);
+
+    renderSectionHeader = ({ section }) =>
+        (<View style={styles.header}>
+            <Text>{section.title}</Text>
+        </View>); 
+
     render() {
         return (
-            <ScrollView>
-                <View style={styles.container1} />
-                <View style={styles.container2} />
-                <View style={styles.container3} />
+            <SectionList
+                sections={sections}
+                renderItem={this.renderRow}
+                renderSectionHeader={this.renderSectionHeader}
+            />
+            // <ScrollView>
+                //<View style={styles.container1} />
+                //<View style={styles.container2} />
+                //<View style={styles.container3} />
             
-            </ScrollView>
-            
+            //</ScrollView>
         );
     }
 }
+const sections = [ 
+    { 
+        id: 0, 
+        title: 'Section 1', 
+        data: 
+            [ 
+             { id: 0, text: 'Item 1' }, 
+             { id: 1, text: 'Item 2' },
+             { id: 2, text: 'Item 3' },
+            ] 
+        },
+
+    { id: 1, title: 'Section 2', 
+        data: 
+        [
+            { id: 3, text: 'Item 4' }, 
+            { id: 4, text: 'Item 5' }
+        ] 
+    }
+    ];
+    
 
 const styles = StyleSheet.create({
     container1: {
         padding: 100, 
         backgroundColor: 'blue'
     },
-    container2: {
-        padding: 100, 
-        backgroundColor: 'red'
+    //container2: {
+        //padding: 100, 
+        //backgroundColor: 'red'
+    //},
+    //container3: {
+        //padding: 100, 
+        //backgroundColor: 'yellow'
+    //},
+    // above is for the scrollview
+    header: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'skyblue',
+        fontWeight: 'bold',
     },
-    container3: {
-        padding: 100, 
-        backgroundColor: 'yellow'
+    row: {
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: 'pink',
+        color: 'black'
     }
 });
 export default TodoScreen;
